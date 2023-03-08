b3d0afa29fc20ff44d85c00a2f7469c01b33c4a9
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-03-07 22:33:11 -0500

    Metodo charCodeAt

--- ejemplosMetodos.js
+++ ejemplosMetodos.js
@@ -1,12 +1,16 @@
-/* Metodo charAt */
+/* Metodo charCodeAt */
 
-// devuelve la posición del carácter que le pasemos como parámetro en el interior de la cadena.
+// devuelve el codigo Unicode de los caracteres que esten en la posición que le pasemos.
 
-// Si la cadena contiene varias ocurrencias del carácter que le pasemos como parámetro, 
-// el método charAt devolverá solamente la posición de la primera de ellas. 
-// Si el carácter no está presente en la cadena, el método charAt devolverá una cadena vacía.
+// El método CharCodeAt es aceptado por las cadenas en JavaScript, 
+// devolviendo el código Unicode de 16 bits del carácter que esté en la posición que le pasemos como parámetro al método.
 
-let cadena = 'JavaScript';
-let caracter = cadena.charAt(5); // 'J'
+let codigo1 = 'JavaScript'.charCodeAt(0) // 74
+let codigo2 = 'JavaScript'.charCodeAt(1) // 97
+let codigo3 = 'JavaScript'.charCodeAt(2) // 118
+let codigo4 = 'JavaScript'.charCodeAt(3) // 97
 
-console.log(caracter)
+console.log(codigo1)
+console.log(codigo2)
+console.log(codigo3)
+console.log(codigo4)
